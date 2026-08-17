50529524ba3163ca6974d56ecb926d58cda34d82
Caelvanost <caelvanost@gmail.com>
2026-08-17 22:54:37 +0200

    build: use v0.4.8 API metadata

--- src/STRPluginMessagingAPISKSEMetadata.cpp
+++ src/STRPluginMessagingAPISKSEMetadata.cpp
@@ -3,7 +3,7 @@
 extern "C" __declspec(dllexport) STRPMSKSE::PluginVersionData SKSEPlugin_Version =
 {
     STRPMSKSE::PluginVersionData::kVersion,
-    STRPMSKSE::kPluginVersion_0_4_7,
+    STRPMSKSE::kPluginVersion_0_4_8,
     "STRPluginMessagingAPI",
     "Caelvanost",
     "",
