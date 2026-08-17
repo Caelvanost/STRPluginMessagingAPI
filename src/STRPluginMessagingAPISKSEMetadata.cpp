5c426f20c1865ff20aef144a45cd5a318b43d4b7
Caelvanost <caelvanost@gmail.com>
2026-08-17 06:26:28 +0200

    build: export modern SKSE metadata for API runtime

---
+++ src/STRPluginMessagingAPISKSEMetadata.cpp
@@ -0,0 +1,14 @@
+#include "SKSEPluginVersionCompat.h"
+
+extern "C" __declspec(dllexport) STRPMSKSE::PluginVersionData SKSEPlugin_Version =
+{
+    STRPMSKSE::PluginVersionData::kVersion,
+    STRPMSKSE::kPluginVersion_0_4_1,
+    "STRPluginMessagingAPI",
+    "Caelvanost",
+    "",
+    0,
+    0,
+    { STRPMSKSE::kRuntime_1_6_1170, 0 },
+    0
+};
