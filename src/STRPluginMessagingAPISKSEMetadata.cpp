5ba5f12a0056f220eef99de836df5e66cf760f65
Caelvanost <caelvanost@gmail.com>
2026-08-18 14:34:28 +0200

    build: bump API metadata to v0.5.1

--- src/STRPluginMessagingAPISKSEMetadata.cpp
+++ src/STRPluginMessagingAPISKSEMetadata.cpp
@@ -3,7 +3,7 @@
 extern "C" __declspec(dllexport) STRPMSKSE::PluginVersionData SKSEPlugin_Version =
 {
     STRPMSKSE::PluginVersionData::kVersion,
-    STRPMSKSE::kPluginVersion_0_5_0,
+    STRPMSKSE::kPluginVersion_0_5_1,
     "STRPluginMessagingAPI",
     "Caelvanost",
     "",
