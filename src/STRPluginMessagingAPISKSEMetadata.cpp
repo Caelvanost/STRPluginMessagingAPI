03e5432167b36c931015ef10780d3b74206de513
Caelvanost <caelvanost@gmail.com>
2026-08-19 15:49:14 +0200

    chore: mark API runtime v0.8.2

--- src/STRPluginMessagingAPISKSEMetadata.cpp
+++ src/STRPluginMessagingAPISKSEMetadata.cpp
@@ -4,7 +4,7 @@
 extern "C" __declspec(dllexport) STRPMSKSE::PluginVersionData SKSEPlugin_Version =
 {
     STRPMSKSE::PluginVersionData::kVersion,
-    STRPMSKSE::kPluginVersion_0_8_1,
+    STRPMSKSE::kPluginVersion_0_8_2,
     "STRPluginMessagingAPI",
     "Caelvanost",
     "",
