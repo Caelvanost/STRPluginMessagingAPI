ad2374fb86efd5dc315a16115cb2fe06d291bfca
Caelvanost <caelvanost@gmail.com>
2026-08-21 12:08:14 +0200

    Report STRPM runtime v0.9.0

--- src/STRPluginMessagingAPISKSEMetadata.cpp
+++ src/STRPluginMessagingAPISKSEMetadata.cpp
@@ -4,7 +4,7 @@
 extern "C" __declspec(dllexport) STRPMSKSE::PluginVersionData SKSEPlugin_Version =
 {
     STRPMSKSE::PluginVersionData::kVersion,
-    STRPMSKSE::kPluginVersion_0_8_3,
+    STRPMSKSE::kPluginVersion_0_9_0,
     "STRPluginMessagingAPI",
     "Caelvanost",
     "",
