39cde7920ecf260879c14f804b3855af64d2cab9
Caelvanost <caelvanost@gmail.com>
2026-09-10 20:27:21 +0200

    chore: add STRPM 0.9.2 version

--- src/SKSEPluginVersionCompat.h
+++ src/SKSEPluginVersionCompat.h
@@ -38,4 +38,5 @@ namespace STRPMSKSE
     inline constexpr std::uint32_t kPluginVersion_0_8_3 = 0x00080003;
     inline constexpr std::uint32_t kPluginVersion_0_9_0 = 0x00090000;
     inline constexpr std::uint32_t kPluginVersion_0_9_1 = 0x00090001;
+    inline constexpr std::uint32_t kPluginVersion_0_9_2 = 0x00090002;
 }
